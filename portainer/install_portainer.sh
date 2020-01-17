432189becb31ec2045f66b0452f6f7475c4d788c
absonggit <37949210+absonggit@users.noreply.github.com>
2020-01-18 06:08:21 +0800

    Update install_portainer.sh

--- portainer/install_portainer.sh
+++ portainer/install_portainer.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+
+path="/opt/portainer"
+
+info()
+{
+    echo -e '\033[32m[INFO]\033[0m ' "$@"
+}
+error()
+{
+    echo -e '\033[31m[INFO]\033[0m ' "$@"
+}
+
+[ -d $path ] ||  mkdir $path
+
+cd $path
+curl -so docker-compose.yml  https://raw.githubusercontent.com/absonggit/tools/master/portainer/docker-compose.yml
+
+docker-compose --version &> /dev/null || {
+    error "docker-compose 没有安装,开始自动执行安装"
+    curl -s https://raw.githubusercontent.com/absonggit/tools/master/docker_compose/install_docker_compose.sh | sh
+}
+info "开始安装portainer"
+docker-compose up -d && info "portainer 安装完成"
+docker-compose ps
