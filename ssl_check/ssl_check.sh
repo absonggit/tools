a7af3f341a49b1a6be4ba256c4cb27bf49523753
absonggit <37949210+absonggit@users.noreply.github.com>
2020-01-18 23:21:36 +0800

    Update ssl_check.sh

--- ssl_check/ssl_check.sh
+++ ssl_check/ssl_check.sh
@@ -2,10 +2,7 @@
 
 host=$1
 port=$2
-end_date=`openssl s_client -host $host -port $port -showcerts </dev/null 2>/dev/null |
-          sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |
-      openssl x509 -text 2>/dev/null |
-      sed -n 's/ *Not After : *//p'`
+end_date=`openssl s_client -host $host -port $port -showcerts </dev/null 2>/dev/null | sed -n '/BEGIN CERTIFICATE/,/END CERT/p' | openssl x509 -text 2>/dev/null | sed -n 's/ *Not After : *//p'`
 
 if [ -n "$end_date" ]
 then
