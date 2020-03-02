99c666ea0eb7764d53b62343e232ac11dfb476cb
absonggit <37949210+absonggit@users.noreply.github.com>
2020-03-02 17:25:43 +0800

    Update ssl_check.sh

--- ssl_check/ssl_check.sh
+++ ssl_check/ssl_check.sh
@@ -20,21 +20,22 @@ do
     esac
 done
 
-if [ ! $host -o $port ];then
+if [ ! $host -o $port ]
+then
     echo "ERROR: -h 或 -p 有误"
     exit
-fi
-end_date=`openssl s_client -host $host -port $port -showcerts </dev/null 2>/dev/null |
-      sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |
-      openssl x509 -text 2>/dev/null |
-      sed -n 's/ *Not After : *//p'`
-
-if [ -n "$end_date" ]
-then
-    end_date_seconds=`date '+%s' --date "$end_date"`
-    now_seconds=`date '+%s'`
-    let  ssl_date=($end_date_seconds-$now_seconds)/24/3600
-    echo "$host的证书还有$ssl_date天过期"
 else
-    echo "ERROR: -h 或 -p 有误"
+    end_date=`openssl s_client -host $host -port $port -showcerts </dev/null 2>/dev/null |
+          sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |
+          openssl x509 -text 2>/dev/null |
+          sed -n 's/ *Not After : *//p'`
+    if [ -n "$end_date" ]
+    then
+        end_date_seconds=`date '+%s' --date "$end_date"`
+        now_seconds=`date '+%s'`
+        let  ssl_date=($end_date_seconds-$now_seconds)/24/3600
+        echo "$host的证书还有$ssl_date天过期"
+    else
+        echo "ERROR: -h 或 -p 有误"
+    fi
 fi
