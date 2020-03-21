969237eccbfbd3f48ecb0a7d04ef6f58d55ccabf
absonggit <37949210+absonggit@users.noreply.github.com>
2020-03-21 10:35:02 +0800

    Update ban_ip.sh

--- cc/ban_ip.sh
+++ cc/ban_ip.sh
@@ -6,13 +6,12 @@
 ## 第一个字段为 [17/Mar/2020:15:01:43 +0800]
 ## 第二个字段为 客户端的真实IP
 ###############################################################################
-
 # 检测的日志路径
 log_path=/home/wwwlogs/app.com.log
 # 阈值（每分钟IP的访问量）
-vpt=500
+vpt=1000
 # 解封时间
-expire_time=1
+expire_time=30
 # 被拒过的IP
 black_ip=/tmp/black_ip_list
 # 获取到达到阈值的IP
@@ -25,10 +24,11 @@ function main() {
        if ! iptables -L INPUT | grep $1 &> /dev/null
        then
        iptables -A INPUT -s $1 -j DROP
+       echo "$(date +'%D-%T') IP: $1 封禁" >> $black_ip
        # 定义临时任务，达到解封时间执行iptables规则
        export ban_ip=$1
        at now +$expire_time minutes << EOF
-echo $ban_ip >> $black_ip
+echo "$(date +'%D-%T') IP: $ban_ip 解封" >> $black_ip
 iptables -D INPUT -s $ban_ip -j DROP
 EOF
        fi
