5fa0dc227cba64dad101575fa09b4c83f7ab717f
absonggit <37949210+absonggit@users.noreply.github.com>
2021-03-09 17:36:00 +0800

    Update install_docker_compose.sh

--- docker_compose/install_docker_compose.sh
+++ docker_compose/install_docker_compose.sh
@@ -8,7 +8,7 @@ info()
 install_docker_compose() {
     latest=`curl -s  https://github.com/docker/compose/tags |grep "<a href=\"\/docker\/compose\/releases\/tag\/"  |awk -F'/|"' '{print $(NF-1)}' | grep -v "-" | head -1`
     info "开始安装 docker-compose $latest"
-    sudo curl -L --progress-bar "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L --progress-bar "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     info "安装完成"
     docker-compose --version
