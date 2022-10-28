10f6c0605d2475827cf774430790dc2ff9a976b4
absonggit <37949210+absonggit@users.noreply.github.com>
2022-10-28 16:25:10 +0800

    Update install_docker_compose.sh

--- docker_compose/install_docker_compose.sh
+++ docker_compose/install_docker_compose.sh
@@ -4,7 +4,7 @@ info()
     echo -e '\033[32m[INFO]\033[0m ' "$@"
 }
 install_docker_compose() {
-    latest=`curl -s https://github.com/docker/compose/tags |grep "<a href=\"\/docker\/compose\/releases\/tag\/" |awk -F'/|"' '{print $(NF-1)}' | grep -v "-" | head -1`
+    latest=`curl -s https://github.com/docker/compose/tags |grep "<a href=\"\/docker\/compose\/releases\/tag\/" | awk '{print $NF}'| awk -F"<|>" 'NR==1{print $2}'`
     info "开始安装 docker-compose $latest"
     sudo curl -L --progress-bar "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
     sudo chmod +x /bin/docker-compose
