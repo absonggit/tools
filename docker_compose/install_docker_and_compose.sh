0a634d697d4868c680b72fc43dad53e34bf0b4a5
absonggit <37949210+absonggit@users.noreply.github.com>
2024-07-29 00:29:31 +0800

    Update install_docker_and_compose.sh
    修改获取tag的方式

--- docker_compose/install_docker_and_compose.sh
+++ docker_compose/install_docker_and_compose.sh
@@ -4,7 +4,7 @@ info()
     echo -e '\033[32m[INFO]\033[0m ' "$@"
 }
 install_docker_compose() {
-    latest=`curl -s https://github.com/docker/compose/tags |grep "<a href=\"\/docker\/compose\/releases\/tag\/" | awk '{print $NF}'| awk -F"<|>" 'NR==1{print $2}'`
+    latest=`curl -s https://github.com/docker/compose/releases |awk -F"\"|/" '$0 ~ "expanded_assets" {print $(NF-1); exit}'`
     info "开始安装 docker-compose $latest"
     sudo curl -L --progress-bar "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
     sudo chmod +x /bin/docker-compose
