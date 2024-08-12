b7b11c7faec8ac62402c2e7cb944f47236af6abf
absonggit <37949210+absonggit@users.noreply.github.com>
2024-08-12 13:24:07 +0800

    Update file_watch_to_tg.py

--- file_watch_to_tg/file_watch_to_tg.py
+++ file_watch_to_tg/file_watch_to_tg.py
@@ -11,12 +11,14 @@ import json
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class FileChangeHandler(FileSystemEventHandler):
-    def __init__(self, target_files, keywords, bot_token, chat_id):
+    def __init__(self, target_files, keywords, bot_token, chat_id, extractors, message_template):
         self.target_files = set(target_files)
         self.keywords = keywords
         self.bot_token = bot_token
         self.chat_id = chat_id
         self.last_modified = {}
+        self.extractors = extractors
+        self.message_template = message_template
         self.file_offsets = {file: self.get_file_size(file) for file in target_files}
 
     def get_file_size(self, file_path):
@@ -28,10 +30,10 @@ class FileChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.is_directory:
             return
-
         if event.src_path in self.target_files:
             if (event.src_path not in self.last_modified):
                 self.print_new_file_content(event.src_path)
+
     def print_new_file_content(self, file_path):
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
@@ -40,27 +42,9 @@ class FileChangeHandler(FileSystemEventHandler):
                 if new_content:
                     for keyword in self.keywords:
                         if keyword in new_content:
-                            message = {}
-                            message['date'] = extractors['date'](new_content)
-                            message['file'] = extractors['file_path'](new_content)
-                            message['line'] = extractors['line_number'](new_content) 
-                            message['id'] = extractors['rule_id'](new_content)
-                            message['ip'] = extractors['client_ip'](new_content)
-                            message['host'] = extractors['host'](new_content)
-                            message['request'] = extractors['request'](new_content)
-                            message['msg'] = extractors['message'](new_content)
-                            text = f"""
-**WAF 通知**
-```
-时间: {message['date']}
-规则ID: {message['id']}
-规则文件: {message['file']} 第 {message['line']} 行
-请求IP: {message['ip']}
-请求域名: {message['host']}
-请求内容: {message['request']}
-拦截消息: {message['msg']}
-```
-"""
+                            message = {key: extractor(new_content) for key, extractor in self.extractors.items()}
+                            escaped_message = {key: escape_markdown(value or '未知') for key, value in message.items()}
+                            text = self.message_template.format(**escaped_message)
                             logging.info(f"{file_path} 匹配到关键字{keyword},发送telegram 通知")
                             send_message_to_telegram(self.bot_token, self.chat_id, text)
                             break
@@ -90,19 +74,15 @@ def send_message_to_telegram(bot_token, chat_id, message):
 def create_extractor(pattern, group_index=1):
     def extractor(log_line):
         match = re.search(pattern, log_line)
+        if not match:
+            logging.error(f"No match found for pattern: '{pattern}' in line: {log_line}")
+        if match and group_index >= len(match.groups()) + 1:
+            logging.error(f"No such group {group_index} for pattern: '{pattern}' in line: {log_line}")
+            return None
         return match.group(group_index) if match else None
     return extractor
-
-extractors = {
-    'date': create_extractor(r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}', 0),
-    'file_path': create_extractor(r'\[file "([^"]+)"\]'),
-    'line_number': create_extractor(r'\[line "(\d+)"\]'),
-    'rule_id': create_extractor(r'\[id "(\d+)"\]'),
-    'client_ip': create_extractor(r'\[client ([\d\.]+)\]'),
-    'request': create_extractor(r'request: "([^"]+)"'),
-    'host': create_extractor(r'host: "([^"]+)"'),
-    'message': create_extractor(r'\[msg "([^"]+)"\]')
-}
+def escape_markdown(text):
+    return re.sub(r'([*_`\[\]()~>#+\-=|{}.!])', r'\\\1', text)
 
 def main():
     config = load_config('config.yaml')
@@ -110,17 +90,19 @@ def main():
     keywords = config['keywords']
     bot_token = config['telegram']['bot_token']
     chat_id = config['telegram']['chat_id']
+    message_template = config['message_template']
+    match_log_field = config['match_log_field']
+    extractors = {
+        key: create_extractor(pattern)
+        for key, pattern in match_log_field.items()
+    }
     logging.info(f"开始监听文件: {target_files}")
-    
     observer = Observer()
-    event_handler = FileChangeHandler(target_files, keywords, bot_token, chat_id)
-
+    event_handler = FileChangeHandler(target_files, keywords, bot_token, chat_id, extractors, message_template)
     directories_to_watch = {file_path.rsplit('/', 1)[0] if '/' in file_path else '.' for file_path in target_files}
-    
     for directory in directories_to_watch:
         observer.schedule(event_handler, path=directory, recursive=False)
     observer.start()
-
     try:
         while True:
             time.sleep(1)
